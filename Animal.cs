7f6a3730b8503967979931423d2c96e515ffd94d
Lydia Jagdeo <99758270+lydiajagdeo@users.noreply.github.com>
2022-08-23 17:40:30 -0400

    Create Animal.cs

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Inheritance
+{
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+
+        public string Eyes { get; set; }
+        public string Heart { get; set; }
+        public string Offsring { get; set; }
+        public string Mouth { get; set; }
+    }
+}
