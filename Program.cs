31d0967f9b88a8773c7c70f89717a7266eb280fa
Lydia Jagdeo <99758270+lydiajagdeo@users.noreply.github.com>
2022-08-23 17:42:34 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO Be sure to follow best practice when creating your classes
+
+            // Create a class Animal
+            // give this class 4 members that all Animals have in common
+
+
+            // Create a class Bird
+            // give this class 4 members that are specific to Bird
+            // Set this class to inherit from your Animal Class
+
+            // Create a class Reptile
+            // give this class 4 members that are specific to Reptile
+            // Set this class to inherit from your Animal Class
+
+
+
+
+            /*Create an object of your Bird class
+             *  give values to your members using the object of your Bird class
+             *  
+             * Creatively display the class member values 
+             */
+            var myBird = new Bird()
+            {
+                HasABeak=true,
+                HasWings= true,
+                HasFeathers=true,
+                LayOnANest=true
+            };
+
+            myBird.BirdFeature();
+
+
+
+
+
+            /*Create an object of your Reptile class
+             *  give values to your members using the object of your Reptile class
+             *  
+             * Creatively display the class member values 
+             */
+            var dinasaur = new Reptilen()
+            {
+                HasATail = true,
+                HasScales = true,
+                IsSlimy = true,
+                LivesInSwamps = false
+            };
+
+            dinasaur.DinasaurFeatures();
+        }
+    }
+}
